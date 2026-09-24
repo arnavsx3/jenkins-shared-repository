697f509f6d37339489700d5c2001a8ce7bec0f88
arnavsx3 <arnavsx3@gmail.com>
2026-09-24 19:42:26 +0000

    implement webhook architecture in SonarQube

--- vars/sonarScan.groovy
+++ vars/sonarScan.groovy
@@ -1,14 +1,11 @@
 def call(String sonarServer, String projectKey) {
     withSonarQubeEnv(sonarServer) {
         sh """
-            docker run --rm \
-                --network devsecops \
-                -v "\$WORKSPACE:/usr/src" \
-                -e SONAR_HOST_URL="\$SONAR_HOST_URL" \
-                -e SONAR_TOKEN="\$SONAR_AUTH_TOKEN" \
-                sonarsource/sonar-scanner-cli:12.2 \
+            \$SONAR_HOME/bin/sonar-scanner \
                 -Dsonar.projectKey=${projectKey} \
-                -Dsonar.sources=/usr/src
+                -Dsonar.sources=.
         """
     }
 }
+
+
