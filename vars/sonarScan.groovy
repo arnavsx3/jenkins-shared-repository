bf864cd43e9fa9d530a21e00717ee8f716e19ee8
arnavsx3 <arnavsx3@gmail.com>
2026-09-24 14:28:03 +0000

    extract tokens from withSonarQubeEnv

--- vars/sonarScan.groovy
+++ vars/sonarScan.groovy
@@ -4,6 +4,8 @@ def call(String sonarServer, String projectKey) {
             docker run --rm \
                 --network devsecops \
                 -v "\$WORKSPACE:/usr/src" \
+                -e SONAR_HOST_URL="\$SONAR_HOST_URL" \
+                -e SONAR_TOKEN="\$SONAR_AUTH_TOKEN" \
                 sonarsource/sonar-scanner-cli:12.2 \
                 -Dsonar.projectKey=${projectKey} \
                 -Dsonar.sources=/usr/src
