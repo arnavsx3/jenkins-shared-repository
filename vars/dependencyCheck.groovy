94bea7b73d89f10ea71fb8b7f2dff03556b1d7c5
arnavsx3 <arnavsx3@gmail.com>
2026-09-25 08:07:21 +0000

    config dependencyCheck.groovy with NVD-key

--- vars/dependencyCheck.groovy
+++ vars/dependencyCheck.groovy
@@ -1,9 +1,14 @@
-def call(String installation) {
-    dependencyCheck(
-        additionalArguments: '--scan ./',
-        odcInstallation: installation
-    )
-    
+def call(String installation, String nvdCredentialId) {
+
+    withCredentials([
+        string(credentialsId: nvdCredentialId, variable: 'NVD_API_KEY')
+    ]) {
+        dependencyCheck(
+            additionalArguments: "--scan ./ --nvdApiKey \$NVD_API_KEY",
+            odcInstallation: installation
+        )
+    }
+
     dependencyCheckPublisher(
         pattern: '**/dependency-check-report.xml'
     )
