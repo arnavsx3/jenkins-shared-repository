e4aeec92113397bde57bddf943d224840b7ec6cd
arnavsx3 <arnavsx3@gmail.com>
2026-09-25 11:23:29 +0000

    generate json result file of osvScan

--- vars/osvScan.groovy
+++ vars/osvScan.groovy
@@ -1,5 +1,12 @@
 def call() {
     sh '''
-        osv-scanner scan source --recursive .
+        osv-scanner scan source --recursive . \
+          --format json \
+          --output-file osv-report.json
     '''
+
+    archiveArtifacts(
+        artifacts: 'osv-report.json',
+        fingerprint: true
+    )
 }
